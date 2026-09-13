e41922095398883692e0d2c409077430f6e49ac3
Jonin9 <130199173+Jonin9@users.noreply.github.com>
2026-09-12 23:32:19 -0500

    Configure installed web app metadata

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,17 @@ import './details.css';
 
 export const metadata = {
   title: 'Hyperbolic Time Chamber',
-  description: 'Japan bulk tracker'
+  description: 'Japan bulk tracker',
+  manifest: '/manifest.webmanifest',
+  appleWebApp: {
+    capable: true,
+    title: 'Time Chamber',
+    statusBarStyle: 'black-translucent'
+  }
+};
+
+export const viewport = {
+  themeColor: '#050816'
 };
 
 export default function RootLayout({ children }) {
